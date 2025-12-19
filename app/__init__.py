4f59f88116c07456a2514a4dfbf32745c526ab16
rodlarca <rodlarca@gmail.com>
2025-12-18 23:35:29 -0300

    cambio en archivo de base de datos

--- app/__init__.py
+++ app/__init__.py
@@ -6,8 +6,8 @@ db = SQLAlchemy()
 def create_app():
     app = Flask(__name__, instance_relative_config=True)
 
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
-    #app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////data/database.db'
+    #app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////data/database.db'
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     app.config['SECRET_KEY'] = 'dev_key_123'
 
